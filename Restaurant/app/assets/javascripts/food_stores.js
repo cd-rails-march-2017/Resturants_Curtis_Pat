5847f9751baf3005e21627166eb765648883e8f2
Pat Falconer <Patrick.j.Falconer@gmail.com>
2017-03-17 13:52:37 -0500

    change

--- Restaurant/app/assets/javascripts/food_stores.js
+++ Restaurant/app/assets/javascripts/food_stores.js
@@ -2,13 +2,15 @@ $(document).ready(function(){
   $('#show_form').click(function(){
     $('#create_form').toggle()
   })
-  $('#create_form').click(function(){
-    $.ajax(function(){
-      url: '/food_stores',
-      type: 'POST',
-      data:
+  $('#create_form').click(function(data){
+    var valuesToSubmit = $(this).serialize();
+    console.log(valuesToSubmit)
+    // $.ajax(function(data){
+    //   url: '/food_stores',
+    //   type: 'POST',
+    //   data:
 
       
-    })
+    // })
   })
 })
